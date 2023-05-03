f1b5e954d660d0b3649467efcf38a4ae796c67f0
Randy Layne <randydavidl78@gmail.com>
2023-05-03 19:45:32 -0400

    Test having specs fail

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello world!!
+          Goodbye world!!
         </p>
       </header>
     </div>
