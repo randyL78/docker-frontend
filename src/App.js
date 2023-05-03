98591003404dc4dd761e81c0ecf8e7e7e146658a
Randy Layne <randydavidl78@gmail.com>
2023-05-03 19:47:39 -0400

    Revert text change to get specs to pass again

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Goodbye world!!
+          Hello world!!
         </p>
       </header>
     </div>
